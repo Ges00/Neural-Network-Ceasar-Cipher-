b1402cf283be0500c957bf6755700e1ed184440b
Ges00 <30725699+Ges00@users.noreply.github.com>
2019-10-03 10:14:14 +0200

    Add files via upload

---
+++ tensor_flow.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow import keras
+#hep libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+#my libraries
+import loading_data as ld
+import frequency as freq
+
+#caricmento dei dati per l'allenaento e per il testing
+#le x sono le frequenze delle lettere di ogni parola cifrata
+#le y sono array di zeri con un uno nella posizione che identifica la chiave corretta
+(x_train, y_train), (x_test, y_test) = ld.load_data()
+#x_train = tf.keras.utils.normalize(x_train,axis=1)
+#x_test = tf.keras.utils.normalize(x_train,axis=1)
+
+#prima cosa da fare, creo un modello incui gli output di un layer sonoinput del seguente
+#simile a tf.model ma quest'ultima è più generica. per una rete base uso sequential
+model = tf.keras.models.Sequential()
+#usato per appiattire multi dimentional array
+#model.add(tf.keras.layers.Flatten())
+
+#layer di input, posso usare anche la relu
+#model.add(tf.keras.models.Flatten())
+model.add(tf.keras.layers.Dense(26, input_dim=8, activation='relu'))
+#hidden layer
+model.add(tf.keras.layers.Dense(26, activation='relu'))
+#output layer
+model.add(tf.keras.layers.Dense(26, activation='sigmoid'))
+
+#cercare la loss giusta
+model.compile(optimizer='adam',
+            loss='mean_squared_error',
+            metrics=['accuracy'])
+#riempimento del modello keras, cercare le epochs e i batch corretti
+model.fit(x_train, y_train, epochs=150, batch_size=10)
+
+# evaluate the keras model
+_, accuracy = model.evaluate(x_train, y_train)
+print('Accuracy: %.2f \n' % (accuracy*100))
