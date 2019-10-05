fd997ac6785a18002dc8c87b6b80506e39275046
Diego Berardi <diegoberardi@gmail.com>
2019-10-05 14:49:00 +0200

    2 hidden layers and saving

--- tensor_flow.py
+++ tensor_flow.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 #my libraries
 import loading_data as ld
 import frequency as freq
+import os
 
 #caricmento dei dati per l'allenaento e per il testing
 #le x sono le frequenze delle lettere di ogni parola cifrata
@@ -24,12 +25,14 @@ model = tf.keras.models.Sequential()
 #layer di input, posso usare anche la relu
 #model.add(tf.keras.models.Flatten())
 model.add(tf.keras.layers.Dense(26, input_dim=26, activation='relu'))
-#hidden layer
-model.add(tf.keras.layers.Dense(26, activation='relu'))
+#first hidden layer
+model.add(tf.keras.layers.Dense(30, activation='relu'))
+#second hidden layer
+model.add(tf.keras.layers.Dense(30, activation='relu'))
 #la sigmoidea ritorna una probabilità
 #la relu è preferibile in quanto riesco ad avvicinarmi di più all'output desiderato
 #output layer
-model.add(tf.keras.layers.Dense(26, activation='relu'))
+model.add(tf.keras.layers.Dense(26, activation='softmax'))
 
 #cercare la loss giusta
 model.compile(optimizer='adam',
@@ -38,6 +41,7 @@ model.compile(optimizer='adam',
 #riempimento del modello keras, cercare le epochs e i batch corretti
 model.fit(x_train, y_train, epochs=150, batch_size=10)
 
+
 # evaluate the keras model
 _, accuracy = model.evaluate(x_train, y_train)
 print('Accuracy: %.2f \n' % (accuracy*100))
@@ -45,4 +49,13 @@ print('Accuracy: %.2f \n' % (accuracy*100))
 #making prediction for the test set
 predictions = model.predict(x_test)
 for i in range(5):
-	print('%s => %d (expected %d)' % (x_test[i].tolist(), predictions[i], y_test[i]))
+	print('%s => %s (expected %s)' % (x_test[i].tolist(), predictions[i], y_test[i]))
+
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
